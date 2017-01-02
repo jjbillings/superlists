f207b52eb350b8ee99474057a30d80a62ffea7a7
jjbillings <jjbillings@noctrl.edu>
2017-01-02 16:42:25 -0600

    Added test for home_page html, defined home_page view function, deleted useless test reminder

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -7,3 +9,12 @@ class HomePageTest(TestCase):
     def testRootURLResolvesToHomePageView(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def testHomePageReturnsGoodHtml(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do Lists</title>',html)
+        self.assertTrue(html.endswith('</html>'))
