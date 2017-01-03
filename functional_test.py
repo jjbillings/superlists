fb17f8f76b4198c9ce5007e35e5f8eb4f9b0ccd4
jjbillings <jjbillings@noctrl.edu>
2017-01-03 14:39:24 -0600

    functional_test checks to see if user can add a new todo item

--- functional_test.py
+++ functional_test.py
@@ -1,18 +1,42 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
+
+    #User opens firefox
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
-    def tearDown(self):
-        self.browser.quit()
-
     def testCanIStartAListAndRetrieveItLater(self):
+        #User loads the To-Do Lists webpage
         self.browser.get("http://localhost:8000")
 
-        self.assertIn('To-Do',self.browser.title)
+        #check that the title is all good
+        self.assertIn('To-Do Lists',self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
 
+        #User is prompted to make a new to-do list item
+        inputBox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputBox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputBox.send_keys('Buy Beer')
+
+        inputBox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == "1: Buy Beer" for row in rows))
+
+        self.fail("Finish Test?")
+
+    #User peaces out.
+    def tearDown(self):
+        self.browser.quit()
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
