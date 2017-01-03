47bf76c62484b582cfb161ec1998dc4adeac4c28
jjbillings <jjbillings@noctrl.edu>
2017-01-03 15:01:22 -0600

    Add more HTML to home_page

--- functional_test.py
+++ functional_test.py
@@ -31,7 +31,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == "1: Buy Beer" for row in rows))
+        self.assertTrue(
+            any(row.text == "1: Buy Beer" for row in rows),
+            "New to-do item did not appear in the table :("
+        )
 
         self.fail("Finish Test?")
 
