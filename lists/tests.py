4a9c61ed534885622a9dd82302fb6910e3cb252b
jjbillings <jjbillings@noctrl.edu>
2017-01-03 14:54:53 -0600

    Refactored unit test to use Django Test Client

--- lists/tests.py
+++ lists/tests.py
@@ -6,15 +6,6 @@ from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def testRootURLResolvesToHomePageView(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def testHomePageReturnsGoodHtml(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do Lists</title>',html)
-        self.assertTrue(html.strip().endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
