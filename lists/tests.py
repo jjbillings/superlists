b3431d02a66db57e89f76fa38a6743e633e89e6f
jjbillings <jjbillings@noctrl.edu>
2017-01-04 14:06:52 -0600

    add unit test for saving items to DB, created basic model for item

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,8 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
+from lists.models import Item
+
 # Create your tests here.
 class HomePageTest(TestCase):
 
@@ -14,3 +16,23 @@ class HomePageTest(TestCase):
         response = self.client.post('/',data={'item_text': 'A new list item'})
         self.assertIn('A new list item',response.content.decode())
         self.assertTemplateUsed(response,'home.html')
+
+class ItemModelTest(TestCase):
+
+    def testSavingAndRetrievingItems(self):
+        firstItem = Item()
+        firstItem.text = 'First List Item'
+        firstItem.save()
+
+        secondItem = Item()
+        secondItem.text = 'Less exciting, second item'
+        secondItem.save()
+
+        savedItems = Item.objects.all()
+        self.assertEqual(savedItems.count(),2);
+
+        firstSavedItem = savedItems[0]
+        secondSavedItem = savedItems[1]
+
+        self.assertEqual(firstSavedItem.text, 'First List Item')
+        self.assertEqual(secondSavedItem.text, 'Less exciting, second item')
